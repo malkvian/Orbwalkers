0ff2bb6783feb25cc8c31c213c5699317e9f9166
malkvian <alysonmelosoares@gmail.com>
2014-12-03 19:16:25 -0300

    league#
    orb

---
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Loader.cs
@@ -0,0 +1,17 @@
+﻿using LeagueSharp;
+
+namespace Ultimate_Carry_Prevolution
+{
+    internal class Loader
+    {
+        public const string VersionNumber = "1.6";
+        public static bool IsBetaTester;
+
+        public Loader()
+        {
+            IsBetaTester = true;
+            Game.PrintChat("BetaTests enabled.");
+            Chat.WellCome();
+        }
+    }
+}
---
+++ Ultimate Carry Prevolution/Ultimate Carry Prevolution/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ultimate_Carry_Prevolution
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Events.Game.OnGameStart += OnGameStart;
+        }
+
+        private static void OnGameStart(EventArgs args)
+        {
+            var l = new Loader();
+        }
+    }
+}
